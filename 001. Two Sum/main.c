ffb5dec0f4118b40025f2203aec9f579a7950c09
Andrey Balamut <balamut.andrey@gmail.com>
2023-01-07 18:26:36 +0300

    Mod 001. Two Sum -- Add main in C

--- 001. Two Sum/main.c
+++ 001. Two Sum/main.c
@@ -2,6 +2,9 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
+// #include <stdlib.h>
+// #include <stdio.h>
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int i = 0;
     int n = 0;
@@ -23,3 +26,18 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize = 0;
     return (0);
 }
+
+// int main() {
+//     int nums[3] = {3, 2, 4};
+//     int numsSize = 3;
+//     int target = 6;
+//     int returnSize;
+//     int *result;
+
+//     result = twoSum(nums, numsSize, target, &returnSize);
+//     if (returnSize > 0) {
+//         printf("[%d %d]\n", result[0], result[1]);
+//         free(&result);
+//     }
+//     return (0);
+// }
